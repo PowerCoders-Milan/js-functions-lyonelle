e2375ebb7cd60d578a7b8e42bbed8ed5beb1f93e
lyonelle <ambouloulyo@gmail.com>
2021-06-16 13:39:07 +0200

    exercise # done

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,26 @@
-// define a function multiplyByNineFifths(number)
+//define a function multiplyByNineFifths(number)
+let number= Math.ceil(Math.random()*6)
+function multiplyByNineFifths  (number)   {
+    return number*(9/5)
+};
+console.log(multiplyByNineFifths(number) )
+// // define a function getFahrenheit(celsius)
+ function getFahrenheit (celsius) {
+    return multiplyByNineFifths(number) + 32;
+}
 
-    //return number*(9/5)
+ // call getFahrenheit();
+console.log(getFahrenheit(5))
 
-// define a function getFahrenheit(celsius)
+// // do the same thing starting from Fahrenheit to Celsius
+let numbers= Math.ceil(Math.random()*4)
+function substrationMinusThirtyTwo(numbers){
+    return ((numbers - 32)*5/9 )
+}
 
-    // return multiplyByNineFifths(celsius) + 32;
+ console.log(substrationMinusThirtyTwo(numbers))
 
-console.log() // call getFahrenheit();
-
-
-// do the same thing starting from Fahrenheit to Celsius
+// function getCelsius (fahrenheit) {
+//     return(substrationMinusThirtyTwo) * 5/9
+// }
+// console.log(getCelsius(4))
